6ec362b9d73c050384d7c74194816508419209ab
nilkanthdhanani <nilkanth286.rejoice@gmail.com>
2024-09-27 17:22:27 +0530

    add details in box

--- src/modules/home/heroBanner/index.js
+++ src/modules/home/heroBanner/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './heroBanner.scss';
 import {
-  mainBlock1, mainBlock2, polly1, polly2, polly3, polly4, polly6, polly7, polly8, polly9, polly10, polly11, polly12, kevin1, kevin2, kevin3, train1, train2, train3, polly5, kevin4, train4
+  mainBlock1, mainBlock2, polly1, polly2, polly3, polly4, polly6, polly7, polly8, polly9, polly10, polly11, polly12, kevin1, kevin2, kevin3, kevin4, train1, train2, train3, train4, polly5
 } from '../../../assets/images/image';
 import UploadIco from '../../../assets/images/svg/uploadIco';
 import RightScrollIco from '../../../assets/images/svg/rightScrollIco';
@@ -33,172 +33,53 @@ export default function HeroBanner() {
     }
   };
 
+  const renderBlockDiv = (images, index, setIndex, altText, h3Text, pText, spanText) => (
+    <div className="block-div">
+      <div className={`block-div-image slide-${direction}`}>
+        <img src={images[index]} alt={`${altText}${index + 1}`} />
+        <div className="upload">
+          <UploadIco />
+        </div>
+        {index > 0 && (
+          <div className="left-scroll">
+            <button type="button" onClick={() => handlePrevImage(setIndex, index)}>
+              <LeftScrollIco />
+            </button>
+          </div>
+        )}
+        {index < images.length - 1 && (
+          <div className="right-scroll">
+            <button type="button" onClick={() => handleNextImage(setIndex, index, images.length)}>
+              <RightScrollIco />
+            </button>
+          </div>
+        )}
+        {images.length > 1 && (
+          <div className="track-dots">
+            {images.map((_, i) => (
+              <span key={i} className={`dot ${index === i ? 'active' : ''}`} />
+            ))}
+          </div>
+        )}
+      </div>
+      <div className="block-div-text">
+        <h3>{h3Text}</h3>
+        <p>{pText}</p>
+        <span>{spanText}</span>
+      </div>
+    </div>
+  );
+
   return (
     <section>
       <div className="container">
         <div className="main-block">
-          <div className="block-div">
-            <div className="block-div-image">
-              <img src={mainBlock1} alt="mainBlock1" />
-              <div className="upload">
-                <UploadIco />
-              </div>
-            </div>
-            <div className="block-div-text">
-              <h3>Stay in Prince’s Purple Rain house</h3>
-              <p>Hosted by Wendy and Lisa</p>
-              <span>Coming September</span>
-            </div>
-          </div>
-          <div className="block-div">
-            <div className="block-div-image">
-              <img src={mainBlock2} alt="mainBlock2" />
-              <div className="upload">
-                <UploadIco />
-              </div>
-            </div>
-            <div className="block-div-text">
-              <h3>Join a living room session with Doja</h3>
-              <p>Hosted by Doja Cat</p>
-              <span>Coming October</span>
-            </div>
-          </div>
-
-          {/* Polly Pocket Section */}
-          <div className="block-div">
-            <div className={`block-div-image slide-${direction}`}>
-              <img src={pollyImages[pollyIndex]} alt={`polly${pollyIndex + 1}`} />
-              <div className="upload">
-                <UploadIco />
-              </div>
-              {pollyIndex > 0 && (
-                <div className="left-scroll">
-                  <button type="button" onClick={() => handlePrevImage(setPollyIndex, pollyIndex)}>
-                    <LeftScrollIco />
-                  </button>
-                </div>
-              )}
-              {pollyIndex < pollyImages.length - 1 && (
-                <div className="right-scroll">
-                  <button type="button" onClick={() => handleNextImage(setPollyIndex, pollyIndex, pollyImages.length)}>
-                    <RightScrollIco />
-                  </button>
-                </div>
-              )}
-              <div className="track-dots">
-                {pollyImages.map((_, index) => (
-                  <span key={index} className={`dot ${pollyIndex === index ? 'active' : ''}`} />
-                ))}
-              </div>
-            </div>
-            <div className="block-div-text">
-              <h3>Sleepover at Polly Pocket’s Compact</h3>
-              <p>Hosted by Polly Pocket</p>
-              <span>Sold out</span>
-            </div>
-          </div>
-
-          {/* Playdate Section */}
-          <div className="block-div">
-            <div className={`block-div-image slide-${direction}`}>
-              <img src={playImages[playIndex]} alt={`play${playIndex + 1}`} />
-              <div className="upload">
-                <UploadIco />
-              </div>
-              {playIndex > 0 && (
-                <div className="left-scroll">
-                  <button type="button" onClick={() => handlePrevImage(setPlayIndex, playIndex)}>
-                    <LeftScrollIco />
-                  </button>
-                </div>
-              )}
-              {playIndex < playImages.length - 1 && (
-                <div className="right-scroll">
-                  <button type="button" onClick={() => handleNextImage(setPlayIndex, playIndex, playImages.length)}>
-                    <RightScrollIco />
-                  </button>
-                </div>
-              )}
-              <div className="track-dots">
-                {playImages.map((_, index) => (
-                  <span key={index} className={`dot ${playIndex === index ? 'active' : ''}`} />
-                ))}
-              </div>
-            </div>
-            <div className="block-div-text">
-              <h3>Playdate at Polly Pocket’s Compact</h3>
-              <p>Hosted by Polly Pocket</p>
-              <span>Sold Out</span>
-            </div>
-          </div>
-
-          {/* Kevin Hart Section */}
-          <div className="block-div">
-            <div className={`block-div-image slide-${direction}`}>
-              <img src={kevinImages[kevinIndex]} alt={`kevin${kevinIndex + 1}`} />
-              <div className="upload">
-                <UploadIco />
-              </div>
-              {kevinIndex > 0 && (
-                <div className="left-scroll">
-                  <button type="button" onClick={() => handlePrevImage(setKevinIndex, kevinIndex)}>
-                    <LeftScrollIco />
-                  </button>
-                </div>
-              )}
-              {kevinIndex < kevinImages.length - 1 && (
-                <div className="right-scroll">
-                  <button type="button" onClick={() => handleNextImage(setKevinIndex, kevinIndex, kevinImages.length)}>
-                    <RightScrollIco />
-                  </button>
-                </div>
-              )}
-              <div className="track-dots">
-                {kevinImages.map((_, index) => (
-                  <span key={index} className={`dot ${kevinIndex === index ? 'active' : ''}`} />
-                ))}
-              </div>
-            </div>
-            <div className="block-div-text">
-              <h3>Go VIP with Kevin Hart</h3>
-              <p>Hosted by Kevin Hart</p>
-              <span>Sold Out</span>
-            </div>
-          </div>
-
-          {/* X-Mansion Section */}
-          <div className="block-div">
-            <div className={`block-div-image slide-${direction}`}>
-              <img src={trainImages[trainIndex]} alt={`train${trainIndex + 1}`} />
-              <div className="upload">
-                <UploadIco />
-              </div>
-              {trainIndex > 0 && (
-                <div className="left-scroll">
-                  <button type="button" onClick={() => handlePrevImage(setTrainIndex, trainIndex)}>
-                    <LeftScrollIco />
-                  </button>
-                </div>
-              )}
-              {trainIndex < trainImages.length - 1 && (
-                <div className="right-scroll">
-                  <button type="button" onClick={() => handleNextImage(setTrainIndex, trainIndex, trainImages.length)}>
-                    <RightScrollIco />
-                  </button>
-                </div>
-              )}
-              <div className="track-dots">
-                {trainImages.map((_, index) => (
-                  <span key={index} className={`dot ${trainIndex === index ? 'active' : ''}`} />
-                ))}
-              </div>
-            </div>
-            <div className="block-div-text">
-              <h3>Train at the X-Mansion</h3>
-              <p>Hosted by Jubilee</p>
-              <span>Sold Out</span>
-            </div>
-          </div>
+          {renderBlockDiv([mainBlock1], 0, null, 'mainBlock1', 'Stay in Prince’s Purple Rain house', 'Hosted by Wendy and Lisa', 'Coming September')}
+          {renderBlockDiv([mainBlock2], 0, null, 'mainBlock2', 'Join a living room session with Doja', 'Hosted by Doja Cat', 'Coming October')}
+          {renderBlockDiv(pollyImages, pollyIndex, setPollyIndex, 'polly', 'Sleepover at Polly Pocket’s Compact', 'Hosted by Polly Pocket', 'Sold out')}
+          {renderBlockDiv(playImages, playIndex, setPlayIndex, 'play', 'Playdate at Polly Pocket’s Compact', 'Hosted by Polly Pocket', 'Sold out')}
+          {renderBlockDiv(kevinImages, kevinIndex, setKevinIndex, 'kevin', 'Go VIP with Kevin Hart', 'Hosted by Kevin Hart', 'Sold out')}
+          {renderBlockDiv(trainImages, trainIndex, setTrainIndex, 'train', 'Train at the X-Mansion', 'Hosted by Jubilee', 'Sold out')}
         </div>
       </div>
     </section>
